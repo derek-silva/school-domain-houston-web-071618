2f0b2cbc924cd55dd155dec665a77cccb28806f0
github username <github email address>
2018-07-19 15:54:11 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+
+class School
+
+attr_reader :school, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !@roster.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    if @roster.keys.include?(grade)
+      @roster[grade]
+    end
+  end
+
+  def sort
+    return_value = {}
+    @roster.each do |grade, students|
+      return_value[grade] = students.sort
+    end
+    return_value
+  end
+
+end
